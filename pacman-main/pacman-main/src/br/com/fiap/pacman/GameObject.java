bc964bd10caee4f4e4df21e799f5fcb8d8130e8a
carlopesil <carlosandrade60@hotmail.com>
2023-03-17 20:32:44 -0300

    ok

--- pacman-main/pacman-main/src/br/com/fiap/pacman/GameObject.java
+++ pacman-main/pacman-main/src/br/com/fiap/pacman/GameObject.java
@@ -4,12 +4,25 @@ public class GameObject {
     private int x;
     private int y;
 
+
     public GameObject(){
 
     }
-    public GameObject(int x;int y){
+    public GameObject(int x; int y){
         this.x= x;
         this.y= y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public void setX(int x){
+        this.x=x
+    }
+    public void setY(int y){
+        this.y=y;
     }
 
 }
